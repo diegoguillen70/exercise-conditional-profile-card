914876aac26765e7e8aa2163b5c4fee645493b01
diegoguillen70 <diegoguillen70@gmail.com>
2023-08-03 09:49:36 -0400

    "updating repository"

--- src/js/app.js
+++ src/js/app.js
@@ -33,11 +33,15 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name === null ? "put your name" : variables.name}</h1>
+          <h2>${variables.role === null ? "put your role" : variables.role}</h2>
+          <h3>${variables.city == null ? "City" : variables.city},${
+    variables.country == null ? "Country" : variables.country
+  } </h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
